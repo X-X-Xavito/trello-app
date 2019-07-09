e89fca14c37dc8d1b90d8a5004286eaacd709e40
X-X-Xavito <kaiquexatalla@gmail.com>
2019-07-09 12:17:50 -0300

    Alterações na estrutura do projeto Para melhor funcionamento futuro foram alteradas as estruturas do projeto

--- system/trello_project/trello_project/urls.py
+++ system/trello_project/trello_project/urls.py
@@ -1,4 +1,4 @@
-"""trello_app URL Configuration
+"""trello_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('trello.urls')),
-    path('admin/', admin.site.urls),    
+    path('admin/', admin.site.urls),
+    path('', include('apps.trello.urls'))
 ]
