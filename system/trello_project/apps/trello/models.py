c79a547c41a1606777f05aa78b6adb0d40324495
X-X-Xavito <kaiquexatalla@gmail.com>
2019-07-09 19:41:39 -0300

    Primeira criação de models: Board, Card e Task Essas models podem sofrer alterações.

--- system/trello_project/apps/trello/models.py
+++ system/trello_project/apps/trello/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    board_title = models.CharField(max_length=200)
+
+class Card(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    card_title = models.CharField(max_length=200)
+
+class Task(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    task_description = models.CharField(max_length=200)
+    task_completed = models.BooleanField(default=False)
