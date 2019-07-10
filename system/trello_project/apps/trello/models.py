20eb9fb3322c1e5bc5b077ea3607f00a6a56b8f0
X-X-Xavito <kaiquexatalla@gmail.com>
2019-07-09 21:42:20 -0300

    Alteração do Readme.md Alteração do Readme com as novas orientação do projeto.

--- system/trello_project/apps/trello/models.py
+++ system/trello_project/apps/trello/models.py
@@ -3,11 +3,21 @@ from django.db import models
 class Board(models.Model):
     board_title = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.board_title
+
 class Card(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
     card_title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.card_title
+
 
 class Task(models.Model):
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
     task_description = models.CharField(max_length=200)
     task_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task_description
