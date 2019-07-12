c1cad346fc3dc3d6e6c0322c1e01a293a01b4c65
X-X-Xavito <kaiquexatalla@gmail.com>
2019-07-12 00:06:53 -0300

    Creation of templates and view modification
    Created 'index.html' and 'create_board.html' in 'apps/trello/templates/trello'
     directory. Modifyied the 'index' view to get all the boards and created
    the 'create_board' view that will be used to create new boards

--- system/trello_project/apps/trello/urls.py
+++ system/trello_project/apps/trello/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'trello'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_board', views.create_board, name='create_board')
 
 
 
--- system/trello_project/apps/trello/views.py
+++ system/trello_project/apps/trello/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Board
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello Word')
+    template = 'trello/index.html'
+    boards_list = Board.objects.all()
+
+    context = {
+        'boards_list': boards_list
+    }
+    return render(request,  template, context)
+
+def create_board(request):
+    template = 'trello/create_board.html'
+    return render(request, template)
